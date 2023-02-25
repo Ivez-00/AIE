9ddeff370a993fe7f6087a19aa4ba9ed4bd8dfc4
Ivez <109906102+Ivez-00@users.noreply.github.com>
2023-02-25 20:34:56 +0800

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -3,7 +3,7 @@
 ARC=`getprop ro.product.cpu.abi`
 
 ui_print "- AI engine for Android Performance Management"
-ui_print "- Beta Stage"
+ui_print "- Beta Stage 2"
 ui_print "- Checking Arch.."
 
 if [ "$ARC" == "arm64-v8a" ]; then
@@ -12,9 +12,11 @@ ui_print "- 64bit Arch"
 
 unzip -o "$ZIPFILE" AIEX64 -d $MODPATH >&2
 unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
+
 file "$MODPATH/AIEX64" >&2
 chmod -R 777 "$MODPATH/AIEX64"
 chmod -R 755 "$MODPATH/service.sh"
+
 rm -rf "$MODPATH/AIEX32"
 
 elif [ "$ARC" == "armeabi-v7a" ]; then
@@ -23,8 +25,10 @@ ui_print "- 32bit Arch"
 
 unzip -o "$ZIPFILE" AIEX32 -d $MODPATH >&2
 unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
+
 file "$MODPATH/AIEX32" >&2
 chmod -R 777 "$MODPATH/AIEX32"
 chmod -R 755 "$MODPATH/service.sh"
+
 rm -rf "$MODPATH/AIEX64"
 fi
