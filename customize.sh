f58d5242d196b06259e571bfa64baf3a04cb250c
Ivez <109906102+Ivez-00@users.noreply.github.com>
2023-03-03 00:36:49 +0800

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -12,12 +12,16 @@ ui_print "- 64bit Arch"
 
 unzip -o "$ZIPFILE" AIEX64 -d $MODPATH >&2
 unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
+unzip -o "$ZIPFILE" AIE64 -d $MODPATH >&2
 
 file "$MODPATH/AIEX64" >&2
 chmod -R 777 "$MODPATH/AIEX64"
 chmod -R 755 "$MODPATH/service.sh"
+chmod -R 777 "$MODPATH/AIE64"
+
 
 rm -rf "$MODPATH/AIEX32"
+rm -rf "$MODPATH/AIE32"
 
 elif [ "$ARC" == "armeabi-v7a" ]; then
 
@@ -25,12 +29,15 @@ ui_print "- 32bit Arch"
 
 unzip -o "$ZIPFILE" AIEX32 -d $MODPATH >&2
 unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
+unzip -o "$ZIPFILE" AIE32 -d $MODPATH >&2
 
 file "$MODPATH/AIEX32" >&2
 chmod -R 777 "$MODPATH/AIEX32"
 chmod -R 755 "$MODPATH/service.sh"
+chmod -R 777 "$MODPATH/AIE32"
 
 rm -rf "$MODPATH/AIEX64"
+rm -rf "$MODPATH/AIE64"
 
 else
 
--- service.sh
+++ service.sh
@@ -8,16 +8,28 @@ done;
 
 A64="$MODPATH/AIEX64"
 A32="$MODPATH/AIEX32"
+A6="$MODPATH/AIE64"
+A3="$MODPATH/AIE32"
+
+SBIN=/system/bin
 
 if [ -e "$A64" ]; then
 
-file "$MODPATH/AIEX64" >&2
-$MODPATH/AIEX64
+file "$A64" >&2
+$A64
+file "$A6" >&2
+cp $A6 -t $SBIN
+
 rm -rf "$A32"
+rm -rf "$A3"
 
 elif [ -e "$A32" ]; then
 
-file "$MODPATH/AIEX32" >&2
-$MODPATH/AIEX32
+file "$A32" >&2
+$A32
+file "$A3" >&2
+cp $A3 -t $SBIN
+
 rm -rf "$A64"
+rm -rf "$A6"
 fi
