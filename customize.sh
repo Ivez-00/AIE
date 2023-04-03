49ab9af80e5a05d892ddcbd9a963def69a6490d6
Ivez <109906102+Ivez-00@users.noreply.github.com>
2023-04-03 10:39:20 +0800

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -1,48 +1,6 @@
-#!/system/bin/sh
 
-ARC=`getprop ro.product.cpu.abi`
+chmod -R 777 $MODPATH/install
 
-ui_print "- AI engine for Android Performance Management"
-ui_print "- Beta Stage 3"
-ui_print "- Checking Arch.."
+file "$MODPATH/install" >&2
 
-if [ "$ARC" == "arm64-v8a" ]; then
-
-ui_print "- 64bit Arch"
-
-unzip -o "$ZIPFILE" AIEX64 -d $MODPATH >&2
-unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
-unzip -o "$ZIPFILE" AIE64 -d $MODPATH >&2
-
-file "$MODPATH/AIEX64" >&2
-chmod -R 777 "$MODPATH/AIEX64"
-chmod -R 755 "$MODPATH/service.sh"
-chmod -R 777 "$MODPATH/AIE64"
-
-
-rm -rf "$MODPATH/AIEX32"
-rm -rf "$MODPATH/AIE32"
-
-elif [ "$ARC" == "armeabi-v7a" ]; then
-
-ui_print "- 32bit Arch"
-
-unzip -o "$ZIPFILE" AIEX32 -d $MODPATH >&2
-unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
-unzip -o "$ZIPFILE" AIE32 -d $MODPATH >&2
-
-file "$MODPATH/AIEX32" >&2
-chmod -R 777 "$MODPATH/AIEX32"
-chmod -R 755 "$MODPATH/service.sh"
-chmod -R 777 "$MODPATH/AIE32"
-
-rm -rf "$MODPATH/AIEX64"
-rm -rf "$MODPATH/AIE64"
-
-else
-
-ui_print "- $ARC"
-ui_print "- Device Arch is not supported"
-abort
-
-fi
+$MODPATH/install
--- service.sh
+++ service.sh
@@ -3,33 +3,30 @@ MODPATH=${0%/*}
 
 while [ "$(getprop sys.boot_completed)" != "1" ];
 do 
-sleep 10
+sleep 30
 done;
 
+A64T="$MODPATH/AIEX64NT"
 A64="$MODPATH/AIEX64"
-A32="$MODPATH/AIEX32"
 A6="$MODPATH/AIE64"
-A3="$MODPATH/AIE32"
-
 SBIN=/system/bin
+
+rm -rf $MODPATH/install
+
+file "$A6" >&2
+
+cp $A6 -t $SBIN
 
 if [ -e "$A64" ]; then
 
 file "$A64" >&2
+
 $A64
-file "$A6" >&2
-cp $A6 -t $SBIN
 
-rm -rf "$A32"
-rm -rf "$A3"
+elif [ -e "$A64T" ]; then
 
-elif [ -e "$A32" ]; then
+file "$A64T" >&2
 
-file "$A32" >&2
-$A32
-file "$A3" >&2
-cp $A3 -t $SBIN
+$A64T
 
-rm -rf "$A64"
-rm -rf "$A6"
 fi
